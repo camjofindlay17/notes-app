ba46bf38a67b09ed22b4ef03381232b2ab62b1fc
camjofindlay17 <75704487+camjofindlay17@users.noreply.github.com>
2021-04-30 00:42:10 -0400

    Add files via upload

---
+++ assets/js/server.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs')
+
+const PORT = 8080;
+
+const handleRequest = (req, res) => {
+    const path = req.url
+
+    switch (path) {
+        case '/notes':
+            fs.readFile(`${__dirname}/notes.html`, (err, data) => {
+                if (err) throw err;
+            
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+              });
+            break
+        default: 
+        fs.readFile(`${__dirname}/index.html`, (err, data) => {
+            if (err) throw err;
+        
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+          });
+      }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, () => {
+  console.log(`Server listening on: http://localhost:${PORT}`);
+});
